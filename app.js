915816d507646ef7f42b594d491c64d838b10f27
Syed <amanxsyed@gmail.com>
2024-05-26 02:23:39 +0500

    Add a function that on click fetch the file

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 // Declare variables for the input and button elements
 const fileInput = document.querySelector("input");
 const downloadBtn = document.querySelector("button");
+
+// Add an event listener to the button element
+// on click of button fetch the file
+downloadBtn.addEventListener("click", e => {
+    e.preventDefault();
+    downloadBtn.innerText = "Downloading file...";
+    fetchFile(fileInput.value);
+});
