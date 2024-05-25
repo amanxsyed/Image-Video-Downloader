e99653581a3e4affb88edec7909fe3b75fa276c8
Syed <amanxsyed@gmail.com>
2024-05-26 02:31:08 +0500

    completed the JS functionality of project

--- app.js
+++ app.js
@@ -9,3 +9,23 @@ downloadBtn.addEventListener("click", e => {
     downloadBtn.innerText = "Downloading file...";
     fetchFile(fileInput.value);
 });
+
+// Fetch the file from the server
+function fetchFile(url){
+    fetch(url)
+    .then(res => res.blob())
+    .then(file => {
+        let tempUrl = URL.createObjectURL(file);
+        const aTag = document.createElement("a");
+        aTag.href = tempUrl;
+        aTag.download = url.replace(/^.*[\\\/]/, '');
+        document.body.appendChild(aTag);
+        aTag.click();
+        downloadBtn.innerText = "Download File";
+        URL.revokeObjectURL(tempUrl);
+        aTag.remove();
+    }).catch(() => {
+        alert("Failed to download file!");
+        downloadBtn.innerText = "Download File";
+    });
+}
